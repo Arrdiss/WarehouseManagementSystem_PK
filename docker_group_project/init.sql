0a911615f438af6517904cd7b7853b2c942bf714
Przemyslaw Serek <przemyslawserek@protonmai.com>
2024-10-19 11:34:12 +0200

    Added Docker.

---
+++ docker_group_project/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+INSERT INTO users (username, email) VALUES
+('johndoe', 'johndoe@example.com'),
+('janedoe', 'janedoe@example.com');
+
