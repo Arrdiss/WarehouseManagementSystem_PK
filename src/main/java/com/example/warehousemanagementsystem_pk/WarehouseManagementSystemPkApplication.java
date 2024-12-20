5a0984bea97b534b919d4490cbd5d7e52a2c05a8
Przemysław Serek <przemyslawserek@proton.me>
2024-12-20 20:58:11 +0100

    Added try statement for session factory.

--- src/main/java/com/example/warehousemanagementsystem_pk/WarehouseManagementSystemPkApplication.java
+++ src/main/java/com/example/warehousemanagementsystem_pk/WarehouseManagementSystemPkApplication.java
@@ -6,36 +6,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.hibernate.cfg.Configuration;
 import static java.lang.System.out;
-		;
 
 @SpringBootApplication
 public class WarehouseManagementSystemPkApplication {
 
 	public static void main(String[] args) {
-		var sessionFactory = new Configuration()
-				.addAnnotatedClass(Product.class)
-				// MariaDB
-				.setProperty(AvailableSettings.JAKARTA_JDBC_URL, "jdbc:mariadb://localhost:3306/warehouse")
-				// Credentials
-				.setProperty(AvailableSettings.JAKARTA_JDBC_USER, "root")
-				.setProperty(AvailableSettings.JAKARTA_JDBC_PASSWORD, "password")
-				// Automatic schema export
-				.setProperty(AvailableSettings.JAKARTA_HBM2DDL_DATABASE_ACTION,
-						Action.SPEC_ACTION_DROP)
-				.buildSessionFactory();
+        try (var sessionFactory = new Configuration()
+                .addAnnotatedClass(Product.class)
+                // MariaDB
+                .setProperty(AvailableSettings.JAKARTA_JDBC_URL, "jdbc:mariadb://localhost:3306/warehouse")
+                // Credentials
+                .setProperty(AvailableSettings.JAKARTA_JDBC_USER, "root")
+                .setProperty(AvailableSettings.JAKARTA_JDBC_PASSWORD, "password")
+                // Automatic schema export
+                .setProperty(AvailableSettings.JAKARTA_HBM2DDL_DATABASE_ACTION,
+                        Action.SPEC_ACTION_DROP)
+                .buildSessionFactory()) {
 
-		// export the inferred database schema
-		sessionFactory.getSchemaManager().exportMappedObjects(true);
+            // export the inferred database schema
+            sessionFactory.getSchemaManager().exportMappedObjects(true);
 
-		// persist an entity
-		sessionFactory.inTransaction(session -> {
-			session.persist(new Product("OIL001", "Synthetic oli 10W-40", 10, Category.Oils));
-		});
+            // persist an entity
+            sessionFactory.inTransaction(session -> {
+                session.persist(new Product("OIL001", "Synthetic oli 10W-40", 10, Category.Oils));
+            });
 
-		// query data using HQL
-		sessionFactory.inSession(session -> {
-			out.println(session.createSelectionQuery("select productName||': '||amount from Product").getSingleResult());
-		});
+            // query data using HQL
+            sessionFactory.inSession(session -> {
+                out.println(session.createSelectionQuery("select productName||': '||amount from Product").getSingleResult());
+            });
+        }
 
 //		SpringApplication.run(WarehouseManagementSystemPkApplication.class, args);
 	}
